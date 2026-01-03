bd35ec35bf63ee3bd6a4781b42c9dfa316761fd5
ThrownLemon <admin@thrownlemon.com>
2026-01-04 06:28:06 +1000

    feat: add packaging script to create .app bundle

---
+++ build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -e
+
+APP_NAME="ClaudeUsagePro"
+BUNDLE_ID="com.sisyphus.ClaudeUsagePro"
+BUILD_DIR=".build/release"
+APP_BUNDLE="$APP_NAME.app"
+CONTENTS_DIR="$APP_BUNDLE/Contents"
+MACOS_DIR="$CONTENTS_DIR/MacOS"
+RESOURCES_DIR="$CONTENTS_DIR/Resources"
+
+echo "Building $APP_NAME in release mode..."
+swift build -c release
+
+echo "Creating .app bundle structure..."
+mkdir -p "$MACOS_DIR"
+mkdir -p "$RESOURCES_DIR"
+
+echo "Copying executable..."
+cp "$BUILD_DIR/$APP_NAME" "$MACOS_DIR/"
+
+echo "Creating Info.plist..."
+cat > "$CONTENTS_DIR/Info.plist" <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>CFBundleExecutable</key>
+    <string>$APP_NAME</string>
+    <key>CFBundleIdentifier</key>
+    <string>$BUNDLE_ID</string>
+    <key>CFBundleName</key>
+    <string>$APP_NAME</string>
+    <key>CFBundlePackageType</key>
+    <string>APPL</string>
+    <key>CFBundleShortVersionString</key>
+    <string>1.0</string>
+    <key>LSMinimumSystemVersion</key>
+    <string>13.0</string>
+    <key>LSUIElement</key>
+    <true/>
+    <key>NSHighResolutionCapable</key>
+    <true/>
+</dict>
+</plist>
+EOF
+
+echo "Build complete: $APP_BUNDLE"
+echo "Note: For notifications to work, you may need to sign the app using 'codesign'."
